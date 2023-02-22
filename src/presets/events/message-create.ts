de39d042179472c48e82b788ab562b5169a1ce19
Spimy <officialspyme@gmail.com>
2023-02-22 23:09:07 +0400

    Add preset message create event

---
+++ src/presets/events/message-create.ts
@@ -0,0 +1,29 @@
+import { Message, KitaClient } from '../../index';
+import { Event } from '../../abstract/event';
+
+export class MessageCreateEvent extends Event {
+  constructor(private client: KitaClient) {
+    super('messageCreate');
+  }
+
+  async execute(message: Message): Promise<any> {
+    const prefix = this.client.prefix;
+    if (!message.content.startsWith(prefix)) return;
+
+    const args = message.content.split(' ');
+    const commandName = args.shift()?.slice(1);
+    if (!commandName) return;
+
+    const command =
+      this.client.classicCommands.get(commandName) ||
+      this.client.classicCommands.get(this.client.aliases.get(commandName) || '');
+
+    if (command) {
+      const success = await command.execute(message, args);
+      if (success) return;
+      return this.client.classicCommands.get('help')?.execute(message, [commandName]);
+    }
+
+    return await this.client.classicCommands.get('help')?.execute(message, []);
+  }
+}
