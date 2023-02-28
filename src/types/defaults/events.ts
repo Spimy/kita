c066309358581bc01b436e56be4ce22db22fcaea
Spimy <officialspyme@gmail.com>
2023-02-28 17:44:31 +0400

    Refactor default ready type to events type

--- src/types/defaults/events.ts
+++ src/types/defaults/events.ts
@@ -1,7 +1,15 @@
-import { DefaultReady } from './events/ready';
-
 export interface DefaultEvents {
   ready?: DefaultReady;
   messageCreate?: { enabled: boolean };
   interactionCreate?: { enabled: boolean };
+}
+
+export interface DefaultReady {
+  enabled: boolean;
+  slashCommands: DefaultSlashCommands;
+}
+
+export interface DefaultSlashCommands {
+  register: boolean;
+  guildId?: string;
 }
